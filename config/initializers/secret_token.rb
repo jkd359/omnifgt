6732eda93e1df4236a0666fda212a78e8c329922
pedrogrande <peter.argent@gmail.com>
2013-05-11 21:49:39 +1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omnifgt::Application.config.secret_token = '8756ef140c170b2403c84eb83ef7af9fd09416e759016e60a52f555581009e351c9619ac2cf32fc4fb1a96eeb4770156f4dd427efdff76ad64d5d2d462208ff7'
